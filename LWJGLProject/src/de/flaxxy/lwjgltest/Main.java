a4b6704d688956a616637d7ba6db972e3f1ad534
Richter Felix <ga67yun@mytum.de>
2014-02-09 21:14:16 +0100

    running

--- LWJGLProject/src/de/flaxxy/lwjgltest/Main.java
+++ LWJGLProject/src/de/flaxxy/lwjgltest/Main.java
@@ -1,10 +1,27 @@
 package de.flaxxy.lwjgltest;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(!Display.isCloseRequested()){
+			Display.update();
+		}
+		Display.destroy();
 	}
 
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.start();
+	}
 }
